2f78678a6e05161465406728779ec298d0e7514e
Sailboy42 <ohimsworth@olin.edu>
2023-09-24 13:51:26 -0400

    Add 3D Model Parts
    Added parts and assembly for scanner assembly

---
+++ 3DScanner/3DScanner.ino
@@ -0,0 +1,71 @@
+/*SHARP GP2Y0A21YK0F IR distance sensor with Arduino and SharpIR library example code. More info: https://www.makerguides.com */
+
+// Include the library:
+#include <SharpIR.h>
+
+// Define model and input pin:
+#define IRPin A0
+#define model 20150
+
+// Create variable to store the distance:
+int distance_cm;
+
+/* Model :
+  GP2Y0A02YK0F --> 20150
+  GP2Y0A21YK0F --> 1080
+  GP2Y0A710K0F --> 100500
+  GP2YA41SK0F --> 430
+*/
+
+// Create a new instance of the SharpIR class:
+SharpIR mySensor = SharpIR(IRPin, model);
+
+void setup() {
+  // Begin serial communication at a baudrate of 9600:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Get a distance measurement and store it as distance_cm:
+  distance_cm = mySensor.distance();
+
+  // Print the measured distance to the serial monitor:
+  Serial.print("Mean distance: ");
+  Serial.print(distance_cm);
+  Serial.println(" cm");
+
+  delay(1000);
+}
+
+/*
+MIT License
+Copyright 2021 Michael Schoeffler (https://www.mschoeffler.com)
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+/*
+* Example source code of an Arduino tutorial on how to control an MG 996R servo motor. 
+*/
+/*
+#include <Servo.h> 
+
+Servo servo; // servo object representing the MG 996R servo
+
+void setup() {
+  servo.attach(3); // servo is wired to Arduino on digital pin 3
+}
+
+void loop() {
+  servo.write(0); // move MG996R's shaft to angle 0°
+  delay(1000); // wait for one second
+  servo.write(45); // move MG996R's shaft to angle 45°
+  delay(1000); // wait for one second 
+  servo.write(90); // move MG996R's shaft to angle 90°
+  delay(1000); // wait for one second
+  servo.write(135); // move MG996R's shaft to angle 135°
+  delay(1000); // wait for one second
+  servo.write(180); // move MG996R's shaft to angle 180°
+  delay(1000); // wait for one second
+}
+*/
